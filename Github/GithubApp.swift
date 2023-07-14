793afac949c3a5f0b3a4408b8bbf221aed6b5124
Akshaya <akshayachander@gmail.com>
2023-07-14 10:17:34 -0400

    Initial Commit

---
+++ Github/GithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubApp.swift
+//  Github
+//
+//  Created by Akshaya Chander on 7/14/23.
+//
+
+import SwiftUI
+
+@main
+struct GithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
